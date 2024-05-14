944e3e29263468436d5db7391e19274018dd20ae
HEEL PATEL <91087524+heelpatel01@users.noreply.github.com>
2024-05-14 11:51:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express=require("express");
 const {connectToMongoDB}=require('./connect')
 const path= require('path')
+const cookieParser=require('cookie-parser')
+
+const {restrictToLoggedinUserOnly,justAuth}=require("./middlewares/auth")
 const urlRoute=require('./routes/url');
-const URL=require('./models/url');
 const staticRoute=require('./routes/staticRouter')
+const userRoute=require('./routes/user')
 
 const app= express();
 const PORT= 8001;
-// const URL = require('./models/url')
+const URL = require('./models/url')
 connectToMongoDB('mongodb://127.0.0.1:27017/short-url').then(()=>{console.log("MONGODB CONNECTED")})
 
 app.set('view engine', 'ejs');
@@ -15,37 +18,11 @@ app.set('views',path.resolve("./views"))
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
+app.use(cookieParser())
 
-
-
-
-// app.get('/test',async(req,res)=>{
-//     const allUrls= await URL.find({});
-//     return res.end(`
-//     <html>
-//     <head>
-//         <title>Your HTML Page</title>
-//     </head>
-//     <body>
-//         <ol>
-//             ${allUrls.map((url) => {
-//                 return `<li>${url.shortId} - ${url.redirectURL} - ${url.visitHistory.length} Short URL:- localhost:8001/url/${url.shortId}</li>`;
-//             })}
-//         </ol>
-//     </body>
-//     </html>
-    
-//     `)
-// })
-
-app.get('/',async(req,res)=>{
-    const allUrls=await URL.find({});
-    return res.render("home",{
-        urls: allUrls,
-    });
-})
-app.use('/url',urlRoute);
-app.use('/',staticRoute)
+app.use('/user',userRoute);
+app.use('/url',restrictToLoggedinUserOnly,urlRoute);
+app.use('/',justAuth,staticRoute);
 
 app.get('/url/:shortId',async(req,res)=>{
     const shortId=req.params.shortId;
